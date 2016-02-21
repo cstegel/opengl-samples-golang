e8957cedf16748063ea4cadfedd75ea8716491cf
Cory Stegelmeier <cstegel@gmail.com>
2016-02-21 14:51:15 -0800

    Copying work over from basic-shaders example

---
+++ basic-textures/main.go
@@ -0,0 +1,169 @@
+package main
+
+/*
+Adapted from this tutorial: http://www.learnopengl.com/#!Getting-started/Shaders
+
+Shows how to pass both position and color as inputs to a shader via a VBO
+*/
+
+import (
+	"log"
+	"runtime"
+	"unsafe"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.1/glfw"
+
+	"github.com/opengl-samples-golang/basic-shaders/gfx"
+)
+
+const windowWidth = 800
+const windowHeight = 600
+
+func init() {
+	// GLFW event handling must be run on the main OS thread
+	runtime.LockOSThread()
+}
+
+func main() {
+	if err := glfw.Init(); err != nil {
+		log.Fatalln("failed to inifitialize glfw:", err)
+	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	window, err := glfw.CreateWindow(windowWidth, windowHeight, "basic shaders", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	// Initialize Glow (go function bindings)
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	window.SetKeyCallback(keyCallback)
+
+	err = programLoop(window)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+ * Creates the Vertex Array Object for a triangle.
+ */
+func createTriangleVAO(vertices []float32, indices []uint32) uint32 {
+
+	var VAO uint32
+	gl.GenVertexArrays(1, &VAO)
+
+	var VBO uint32
+	gl.GenBuffers(1, &VBO)
+
+	var EBO uint32;
+	gl.GenBuffers(1, &EBO)
+
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointers()
+	gl.BindVertexArray(VAO)
+
+	// copy vertices data into VBO (it needs to be bound first)
+	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
+	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
+
+	// copy indices into element buffer
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(indices)*4, gl.Ptr(indices), gl.STATIC_DRAW)
+
+	// position
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 6*4, gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(0)
+
+	// color
+	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 6*4, gl.PtrOffset(3*4))
+	gl.EnableVertexAttribArray(1)
+
+	// unbind the VAO (safe practice so we don't accidentally (mis)configure it later)
+	gl.BindVertexArray(0)
+
+	return VAO
+}
+
+func programLoop(window *glfw.Window) error {
+
+	// the linked shader program determines how the data will be rendered
+	vertShader, err := gfx.NewShaderFromFile("shaders/basic.vert", gl.VERTEX_SHADER)
+	if err != nil {
+		return err
+	}
+
+	fragShader, err := gfx.NewShaderFromFile("shaders/basic.frag", gl.FRAGMENT_SHADER)
+	if err != nil {
+		return err
+	}
+
+	shaderProgram, err := gfx.NewProgram(vertShader, fragShader)
+	if err != nil {
+		return err
+	}
+	defer shaderProgram.Delete()
+
+	vertices := []float32{
+		// top
+		0.0, 0.5, 0.0,     // position
+		1.0, 0.0, 0.0,     // Color
+
+		// bottom right
+		0.5, -0.5, 0.0,
+		0.0, 1.0, 0.0,
+
+		// bottom left
+		-0.5, -0.5, 0.0,
+		0.0, 0.0, 1.0,
+	}
+
+	indices := []uint32{
+		0, 1, 2,  // only triangle
+	}
+
+	VAO := createTriangleVAO(vertices, indices)
+
+	for !window.ShouldClose() {
+		// poll events and call their registered callbacks
+		glfw.PollEvents()
+
+		// perform rendering
+		gl.ClearColor(0.2, 0.5, 0.5, 1.0)
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		// draw loop
+
+		// draw triangle
+		shaderProgram.Use()
+		gl.BindVertexArray(VAO)
+		gl.DrawElements(gl.TRIANGLES, 3, gl.UNSIGNED_INT, unsafe.Pointer(nil))
+		gl.BindVertexArray(0)
+
+		// end of draw loop
+
+		// swap in the rendered buffer
+		window.SwapBuffers()
+	}
+
+	return nil
+}
+
+func keyCallback(window *glfw.Window, key glfw.Key, scancode int, action glfw.Action,
+	mods glfw.ModifierKey) {
+
+	// When a user presses the escape key, we set the WindowShouldClose property to true,
+	// which closes the application
+	if key == glfw.KeyEscape && action == glfw.Press {
+		window.SetShouldClose(true)
+	}
+}
